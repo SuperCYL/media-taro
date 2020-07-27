8fd907c4cfe880ad22f5c3dd61b41a21b08fcf7c
曹艳玲 <caoyanling@caoyanlingdeMacBook-Pro.local>
2020-07-27 14:00:39 +0800

    跳转

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import { View, Text } from '@tarojs/components'
+import Taro from '@tarojs/taro'
+import { View } from '@tarojs/components'
 import { AtButton } from 'taro-ui'
 
 import "taro-ui/dist/style/components/button.scss" // 按需引入
@@ -21,16 +22,15 @@ export default class Index extends Component {
   componentDidShow () { }
 
   componentDidHide () { }
-
+  gototest(){
+    Taro.navigateTo({
+      url: '/pages/test/index'
+    })
+  }
   render () {
     return (
       <View className='index'>
-        <Text>Hello world!</Text>
-        <AtButton type='primary'>I need Taro UI</AtButton>
-        <Text>Taro UI 支持 Vue 了吗？</Text>
-        <AtButton type='primary' circle={true}>支持</AtButton>
-        <Text>共建？</Text>
-        <AtButton type='secondary' circle={true}>来</AtButton>
+        <AtButton type='primary' onClick={this.gototest.bind(this)} circle={true}>跳转test</AtButton>
       </View>
     )
   }
