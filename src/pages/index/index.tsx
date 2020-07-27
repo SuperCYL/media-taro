7b63519233072f5635a8de684052623845209b7e
luchen <813502730@qq.com>
2020-07-27 13:48:20 +0800

    update

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,31 +1,45 @@
 import React, { Component } from 'react'
+import Taro from '@tarojs/taro'
 import { View, Text } from '@tarojs/components'
 import { AtButton } from 'taro-ui'
+import { AtNavBar } from 'taro-ui'
 
-import "taro-ui/dist/style/components/button.scss" // 按需引入
 import './index.scss'
 
 export default class Index extends Component {
 
-  componentWillMount () { }
+  componentWillMount() { }
 
-  componentDidMount () { }
+  componentDidMount() { }
 
-  componentWillUnmount () { }
+  componentWillUnmount() { }
 
-  componentDidShow () { }
+  componentDidShow() { }
 
-  componentDidHide () { }
+  componentDidHide() { }
 
-  render () {
+  click() {
+    Taro.navigateTo({
+      url: '/pages/login/login'
+    })
+  }
+
+  render() {
     return (
       <View className='index'>
-        <Text>Hello world!</Text>
+        <AtNavBar
+          color='#000'
+          title='NavBar 导航栏示例'
+          leftText='返回'
+          rightFirstIconType='bullet-list'
+          rightSecondIconType='user'
+        />
+        <Text >Hello world!</Text>
         <AtButton type='primary'>I need Taro UI</AtButton>
         <Text>Taro UI 支持 Vue 了吗？</Text>
         <AtButton type='primary' circle={true}>支持</AtButton>
         <Text>共建？</Text>
-        <AtButton type='secondary' circle={true}>来</AtButton>
+        <AtButton onClick={this.click} type='secondary' circle={true}>跳到登录</AtButton>
       </View>
     )
   }
